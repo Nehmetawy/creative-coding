119a8924695424d699c9f4d0facf83e95a5581cf
anurag fogawat <fogawatanurag@gmail.com>
2024-11-01 22:34:51 +0530

    basic

--- src/code/features/test/index.tsx
+++ src/code/features/test/index.tsx
@@ -2,17 +2,27 @@
 import { P5Canvas } from '@/code/blocks/p5';
 import p5 from 'p5';
 
+const CENTER_W = 400;
+const CENTER_H = 400;
+
 export const sketch = (p: p5) => {
   const windowWidth = window.innerWidth;
   const windowheight = window.innerHeight;
 
+  let pos = p.createVector(CENTER_W, CENTER_H);
+  let p1 = p.createVector(2, 2);
+
   p.setup = () => {
-    p.createCanvas(400, 400);
+    p.createCanvas(CENTER_W * 2, CENTER_H * 2);
     p.background(200);
+    // p.point(pos);
   };
 
   p.draw = () => {
-    p.fill(255, 0, 0);
+    pos.add(p1);
+    p.background(200);
+    p.stroke(5);
+    p.point(pos);
   };
 };
 
